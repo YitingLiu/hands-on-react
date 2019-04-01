b459eab5200e9d0da4d87f4625aed5e6c34c08f4
YitingLiu <yl3799@nyu.edu>
2019-04-01 23:56:01 +0800

    Updates

---
+++ precache-manifest.87395fb864e85df94b570ab2250b78d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hands-on-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6b7d4a09388eca2be5cf",
+    "url": "/hands-on-react/static/js/runtime~main.1539fe58.js"
+  },
+  {
+    "revision": "d21b3adb68ee506ce759",
+    "url": "/hands-on-react/static/js/main.e7946fce.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/hands-on-react/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d21b3adb68ee506ce759",
+    "url": "/hands-on-react/static/css/main.7c933af7.chunk.css"
+  },
+  {
+    "revision": "6b6d4e989e62f2626cdb45295e8ee7d4",
+    "url": "/hands-on-react/index.html"
+  }
+];
